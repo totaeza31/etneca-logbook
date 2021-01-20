28bae4ea55a0b4581b4df86d643ffa321c8207a1
Krittamet Wongsakornpiriya <kritmet.w@gmail.com>
2021-01-20 11:17:50 +0700

    Respon Lowwercase Json

--- models/message.go
+++ models/message.go
@@ -1,50 +1,13 @@
 package models
 
-import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
-type User struct {
-	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	FirstName   firstName          `bson:"firstname"`
-	LastName    lastName           `bson:"lastname"`
-	Address     address            `bson:"address"`
-	Brithday    time.Time
-	Fax         string
-	Email       string
-	Password    string      `json:"password,omitempty" bson:"password,omitempty"`
-	Tel         string      `json:"telephone,omitempty" bson:"telephone,omitempty"`
-	NamePost    namePost    `bson:"namePost"`
-	AddressPost addressPost `bson:"addressPost"`
-	Picture     string
+type Respond struct {
+	Message Message `json:"message"`
+	Result  bool    `json:"result"`
+	Data    Authen  `json:"data"`
 }
 
-type firstName struct {
-	Th string
-	En string
-	Bu string
-}
-
-type lastName struct {
-	Th string
-	En string
-	Bu string
-}
-type address struct {
-	Th string
-	En string
-	Bu string
-}
-
-type namePost struct {
-	Th string
-	En string
-	Bu string
-}
-type addressPost struct {
-	Th string
-	En string
-	Bu string
+type Message struct {
+	Th string `json:"th"`
+	En string `json:"en"`
+	Bu string `json:"bu"`
 }
--- models/user.go
+++ models/user.go
@@ -1,13 +1,50 @@
 package models
 
-type Respond struct {
-	Message Message
-	Result  bool "true"
-	Data    Authen
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FirstName   firstName          `json:"firstname"`
+	LastName    lastName           `json:"lastname"`
+	Address     address            `json:"address"`
+	Brithday    time.Time          `json:"brithday"`
+	Fax         string             `json:"fax"`
+	Email       string             `json:"email"`
+	Password    string             `json:"password,omitempty" bson:"password,omitempty"`
+	Tel         string             `json:"telephone,omitempty" bson:"telephone,omitempty"`
+	NamePost    namePost           `json:"namePost"`
+	AddressPost addressPost        `json:"addressPost"`
+	Picture     string
 }
 
-type Message struct {
-	Th string	"ล็อกอินสำเร็จ"
-	En string	"login success"
-	Bu string
+type firstName struct {
+	Th string `json:"th"`
+	En string `json:"en"`
+	Bu string `json:"bu"`
+}
+
+type lastName struct {
+	Th string `json:"th"`
+	En string `json:"en"`
+	Bu string `json:"bu"`
+}
+type address struct {
+	Th string `json:"th"`
+	En string `json:"en"`
+	Bu string `json:"bu"`
+}
+
+type namePost struct {
+	Th string `json:"th"`
+	En string `json:"en"`
+	Bu string `json:"bu"`
+}
+type addressPost struct {
+	Th string `json:"th"`
+	En string `json:"en"`
+	Bu string `json:"bu"`
 }
