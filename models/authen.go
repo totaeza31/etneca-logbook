70204e628c7561a38b147bda14256d58fda40015
Krittamet Wongsakornpiriya <kritmet.w@gmail.com>
2021-01-20 11:21:54 +0700

    login respon name

--- models/authen.go
+++ models/authen.go
@@ -6,6 +6,8 @@ type Authen struct {
 	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Email        string             `json:"email,omitempty" bson:"email,omitempty"`
 	Password     string             `json:"password,omitempty" bson:"password,omitempty"`
+	FirstName    firstName          `json:"firstname"`
+	LastName     lastName           `json:"lastname"`
 	AccessToken  string             `json:"accessToken,omitempty" bson:"accessToken,omitempty"`
 	RefreshToken string             `json:"refreshToken,omitempty" bson:"refreshToken,omitempty"`
 }
