8c12b39c04e98a3f6fe6682432e62e366dc92e55
Krittamet Wongsakornpiriya <kritmet.w@gmail.com>
2021-02-05 13:41:26 +0700

    Update technician.go

--- route/v1/technician.go
+++ route/v1/technician.go
@@ -9,7 +9,7 @@ import (
 func TechnicianRoute(router *mux.Router) {
 	router.HandleFunc("/tech", controllers.VerifyAccess(controllers.GetTech)).Methods("GET")
 	router.HandleFunc("/tech/{id}", controllers.VerifyAccess(controllers.GetTechByID)).Methods("GET")
-	router.HandleFunc("/tech", VerifyAccess(controllers.PostTech)).Methods("POST")
+	router.HandleFunc("/tech", controllers.VerifyAccess(controllers.PostTech)).Methods("POST")
 	router.HandleFunc("/tech/{id}", controllers.VerifyAccess(controllers.PutTech)).Methods("PUT")
 	router.HandleFunc("/tech/{id}", controllers.VerifyAccess(controllers.DelTech)).Methods("DELETE")
 }
