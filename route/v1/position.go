103593339f5d3b12b4ba638fcb86729856edd400
Krittamet Wongsakornpiriya <kritmet.w@gmail.com>
2021-02-12 09:54:50 +0700

    Update position.go

--- route/v1/position.go
+++ route/v1/position.go
@@ -7,9 +7,9 @@ import (
 )
 
 func PositionRoute(router *mux.Router) {
-	router.HandleFunc("/pst", controllers.VerifyAccess(controllers.GetPositions)).Methods("GET")
-	router.HandleFunc("/pst/{id}", controllers.VerifyAccess(controllers.GetPosition)).Methods("GET")
-	router.HandleFunc("/pst", controllers.VerifyAccess(controllers.PostPosition)).Methods("POST")
-	router.HandleFunc("/pst/{id}", controllers.VerifyAccess(controllers.PutPosition)).Methods("PUT")
-	router.HandleFunc("/pst/{id}", controllers.VerifyAccess(controllers.DelPosition)).Methods("DELETE")
+	router.HandleFunc("/pst", controllers.GetPositions).Methods("GET")
+	router.HandleFunc("/pst/{id}", controllers.GetPosition).Methods("GET")
+	router.HandleFunc("/pst", controllers.PostPosition).Methods("POST")
+	router.HandleFunc("/pst/{id}", controllers.PutPosition).Methods("PUT")
+	router.HandleFunc("/pst/{id}", controllers.DelPosition).Methods("DELETE")
 }
